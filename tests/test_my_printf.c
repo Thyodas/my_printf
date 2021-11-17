f9baba11de4f8cb72f0bd090aa5c8c8bead72701
Thyodas <guillaume.hein@epitech.eu>
2021-11-17 18:31:35 +0100

    Added some tests :)

--- tests/test_my_printf.c
+++ tests/test_my_printf.c
@@ -47,8 +47,74 @@ Test(my_printf, int_format, .init = redirect_all_std)
     cr_assert_stdout_eq_str("hello 1234\n");
 }
 
+Test(my_printf, int_format2, .init = redirect_all_std)
+{
+    my_printf("hello %i\n", 1234);
+    cr_assert_stdout_eq_str("hello 1234\n");
+}
+
 Test(my_printf, int_format_space, .init = redirect_all_std)
 {
     my_printf("hello %10d\n", 1234);
     cr_assert_stdout_eq_str("hello       1234\n");
+}
+
+Test(my_printf, int_format_space_positive, .init = redirect_all_std)
+{
+    my_printf("hello % d\n", 1234);
+    cr_assert_stdout_eq_str("hello  1234\n");
+}
+
+Test(my_printf, int_format_plus_positive, .init = redirect_all_std)
+{
+    my_printf("hello %+d\n", 1234);
+    cr_assert_stdout_eq_str("hello +1234\n");
+}
+
+Test(my_printf, int_format_space_left, .init = redirect_all_std)
+{
+    my_printf("hello %-10d\n", 1234);
+    cr_assert_stdout_eq_str("hello 1234      \n");
+}
+
+Test(my_printf, mix_str_int, .init = redirect_all_std)
+{
+    my_printf("hello %d %s\n", 1234, "hey");
+    cr_assert_stdout_eq_str("hello 1234 hey\n");
+}
+
+Test(my_printf, hex_prefix, .init = redirect_all_std)
+{
+    my_printf("aba%#xdad", 1234);
+    cr_assert_stdout_eq_str("aba0x4d2dad");
+}
+
+Test(my_printf, HEX_prefix, .init = redirect_all_std)
+{
+    my_printf("aba%#Xdad", 1234);
+    cr_assert_stdout_eq_str("aba0X4D2dad");
+}
+
+Test(my_printf, binary, .init = redirect_all_std)
+{
+    my_printf("aba%bdad", 254);
+    cr_assert_stdout_eq_str("aba11111110dad");
+}
+
+Test(my_printf, octal_prefix, .init = redirect_all_std)
+{
+    my_printf("aba%#odad", 1234);
+    cr_assert_stdout_eq_str("aba02322dad");
+}
+
+Test(my_printf, unsigned_int, .init = redirect_all_std)
+{
+    my_printf("aba%udad", -1234);
+    cr_assert_stdout_eq_str("aba4294966062dad");
+}
+
+Test(my_printf, modulo, .init = redirect_all_std)
+{
+    my_printf("aba%+-0#%dad");
+    cr_assert_stdout_eq_str("aba%dad");
 }
